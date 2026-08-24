75d99bff168fc0dcc8d6eb05996ce83c1be167e6
chillingcapi <chillingcapy3@gmail.com>
2026-08-24 18:57:54 +0200

    Update Tester Main.lua

--- Tester Main.lua
+++ Tester Main.lua
@@ -8,8 +8,8 @@ if game.CreatorId == 5212858 then
     end
 end
 
-pcall(function()
+--[[pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chillingcapi/InFamous/refs/heads/main/Loading.lua"))()
-end)
+end)--]]
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9bb63babcc39e4ca6c45fda7041b9ab0.lua"))()
