0060e0133b94eaffdea0450c75bab7d74ded9a55
chillingcapi <chillingcapy3@gmail.com>
2026-08-24 18:57:43 +0200

    Update Public Main.lua

--- Public Main.lua
+++ Public Main.lua
@@ -8,8 +8,8 @@ if game.CreatorId == 5212858 then
     end
 end
 
-pcall(function()
+--[[pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chillingcapi/InFamous/refs/heads/main/Loading.lua"))()
-end)
+end)--]]
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/bbb9e167f6d5801e97294a2b01dd1fa6.lua"))()
