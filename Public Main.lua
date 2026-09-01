f8c5aaa4178f4c8f4f46f22aefe7593e58f2fc74
chillingcapi <chillingcapy3@gmail.com>
2026-09-01 19:40:01 +0200

    Update Public Main.lua

--- Public Main.lua
+++ Public Main.lua
@@ -1,13 +1,5 @@
 -- public
 
-if game.CreatorId == 5212858 then
-    local Executor = identifyexecutor and identifyexecutor() or ""
-    if Executor == "Real" then
-        game:GetService("Players").LocalPlayer:Kick("Evenesce did not load.\n\nReal gets you banned from Deepwoken. Use a different executor for this game.")
-        return
-    end
-end
-
 --[[pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chillingcapi/InFamous/refs/heads/main/Loading.lua"))()
 end)--]]
