7006f342858ee9acd42af98793c9777a3dccbf15
chillingcapi <chillingcapy3@gmail.com>
2026-09-25 21:14:31 +0200

    Update Public Main.lua

--- Public Main.lua
+++ Public Main.lua
@@ -1,7 +1,8 @@
 -- public
 
---[[pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/chillingcapi/InFamous/refs/heads/main/Loading.lua"))()
-end)--]]
+if (game.CreatorId == 12851171) then 
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/cc60cfe2aea52425a64c77e36e611e6d.lua"))()
+    return 
+end
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/bbb9e167f6d5801e97294a2b01dd1fa6.lua"))()
